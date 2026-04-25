841182e0d1f8173035c669839644d1d189adeef2
JoshRuds <124031454+JoshRuds@users.noreply.github.com>
2026-04-25 08:42:25 +0100

    migrate to new tokenisation algorithms

--- src/lexer.c
+++ src/lexer.c
@@ -3,23 +3,6 @@
 #include <stdbool.h>
 #include <ctype.h>
 
-Token* lex(char *code, int len) {
-    char **tokens;
-    int tokens_count = 0;
-    char *tok;
-    int tok_count = 0;
-    for (int i; i < len; i++) {
-        if (code[i] == ' ') {
-            tokens[tokens_count] = tok;
-            tokens_count++;
-            tok = "";
-        } else {
-            tok[tok_count] = code[i];
-            tok_count++;
-        }
-    }
-}
-
 bool strIn(char c, char *toCheck) {
     for (int i = 0; toCheck[i] != '\0'; i++) {
         if (c == toCheck[i]) { return true; }
