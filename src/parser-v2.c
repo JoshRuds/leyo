b5dea0b02b5522f0762ddd0049b88aad6546b3dc
JoshRuds <joshua.ruddick@icloud.com>
2026-05-02 08:03:47 +0100

    bytecoder work init

--- src/parser-v2.c
+++ src/parser-v2.c
@@ -5,12 +5,22 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 typedef struct {
     Token *tokens;
     int pos;
     int count;
-} Parser;
+    uint8_t bytebuff[2048];
+    int byteIndex;
+    int hasHalfByte;
+    uint8_t halfByte;
+} ByteCoder;
+
+typedef struct {
+    uint8_t *data;
+    int length;
+} ByteCodeResult;
 
 char *VARDEFS[] = {
     "str",
@@ -19,20 +29,21 @@ char *VARDEFS[] = {
 };
 int VARDEFCOUNT = 3;
 
-Parser parser = {0};
-Parser *p;
-Program program = {0};
+ByteCoder bytecoder = {0};
+ByteCoder *b;
+
+
 
 static Token current() {
-    return p->tokens[p->pos];
+    return b->tokens[b->pos];
 }
 
 static Token peek() {
-    return p->tokens[p->pos+1];
+    return b->tokens[b->pos+1];
 }
 
 static void advance() {
-    p->pos++;
+    b->pos++;
 }
 
 static void expect(TokenType type, char *errorStr) {
@@ -50,6 +61,26 @@ static void expectAndPass(TokenType type, char *errorStr) {
     advance();
 }
 
+static void writeByte(uint8_t value) {
+    if (b->byteIndex >= sizeof(b->bytebuff)) {
+        raise("Byte buffer overflow", current().line, current().collumn);
+    }
+
+    b->bytebuff[b->byteIndex] = value;
+    b->byteIndex++;
+}
+
+static void writeHexValue(uint8_t value, int digits) {
+    if (!b->hasHalfByte) {
+        b->halfByte = value & 0xF;
+        b->hasHalfByte = 1;
+    } else {
+        uint8_t full = (b->halfByte << 4) | (value & 0xF);
+        writeByte(full);
+        b->hasHalfByte = 0;
+    }
+}
+
 static bool strInVarDef(char *str) {
     for (int i = 0; i < VARDEFCOUNT; i++) {
         if (strcmp(VARDEFS[i], str) == 0) {
@@ -57,4 +88,31 @@ static bool strInVarDef(char *str) {
         }
     }
     return false;
+}
+
+void parseStatement() {
+
+}
+
+ByteCodeResult parseToByteCode(TokenStream ts) {
+    b = &bytecoder;
+    b->tokens = ts.stream;
+    b->pos = 0;
+    b->byteIndex = 0;
+    b->hasHalfByte = 0;
+
+    while (current().type != ENDOFSTREAM) {
+        parseStatement();
+    }
+
+
+
+    if (b->hasHalfByte) {
+        raise("Unpaired half-byte", current().line, current().collumn);
+        callAllErr();
+    }
+    ByteCodeResult res = {0};
+    res.data = b->bytebuff;
+    res.length = b->byteIndex;
+    return res;
 }
