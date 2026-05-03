74fbf476a56a83b9f41347bdf863b76c68a89dcd
JoshRuds <joshua.ruddick@icloud.com>
2026-05-03 08:20:26 +0100

    parser v2 migration

---
+++ archive/parserOLD.c
@@ -0,0 +1,285 @@
+
+#include "../include/type.h"
+#include "../include/errors.h"
+#include "../include/nodes.h"
+#include <ctype.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DEBUG true
+#define LOG(fmt, ...) \
+    if (DEBUG) printf("[LOG] " fmt "\n", ##__VA_ARGS__)
+
+static ASTNode *parseExpression(int minPrec);
+static ASTNode *parseAtom();
+
+typedef struct {
+    Token *tokens;
+    int pos;
+    int count;
+} Parser;
+
+static char *VARDEFS[] = {
+    "str",
+    "int",
+    "flt",
+};
+static int VARDEFCOUNT = 3;
+
+
+
+Parser parser;
+Parser *p;
+Program program = {0};
+
+
+static Token current() {
+    LOG("current");
+    return p->tokens[p->pos];
+}
+
+static Token peek() {
+    LOG("peek");
+    return p->tokens[p->pos + 1];
+}
+
+static void advance() {
+    LOG("advnce");
+    p->pos++;
+    LOG("PROGRAM LOOP pos=%d token=%s", p->pos, current().value);
+}
+
+static void expect(TokenType type, char *errorStr) {
+    LOG("except");
+    if (type != peek().type) {
+        raise(errorStr, peek().line, peek().collumn);
+    }
+    advance();
+}
+
+
+static bool strIsVarDef(char *str) {
+    for (int i = 0; i < VARDEFCOUNT; i++) {
+        if (strcmp(VARDEFS[i], str) == 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+static int precedence(Token tok) {
+    if (tok.type != OPERATION) return -1;
+
+    if (strcmp(tok.value, "+") == 0) return 1;
+    if (strcmp(tok.value, "-") == 0) return 1;
+    if (strcmp(tok.value, "*") == 0) return 2;
+    if (strcmp(tok.value, "/") == 0) return 2;
+
+    return -1;
+}
+
+
+static ASTNode *parseAtom() { // smallest possible node
+    LOG("parse atom");
+    Token tok = current();
+    ASTNode *node = malloc(sizeof(ASTNode));
+
+    switch (tok.type) {
+        case NUMBER:
+            node->type = AST_NUMBER;
+            node->value = tok.value;
+            advance();
+            return node;
+
+        case IDENTIFIER:
+            node->type = AST_IDENTIFIER;
+            node->value = tok.value;
+            advance();
+            return node;
+
+        case OPENBRAC: // '('
+            advance();
+
+            ASTNode *expr = parseExpression(0);
+
+            if (current().type != CLOSEBRAC) {
+                raise("Expected ')'", tok.line, tok.collumn);
+                exit(1);
+            }
+
+            advance();
+            return expr;
+
+        default:
+            raise("Expected expression", tok.line, tok.collumn);
+            exit(1);
+    }
+}
+
+static ASTNode *parseExpression(int minPrec) {
+    LOG("parse expr");
+    ASTNode *left = parseAtom();
+
+    while (true) {
+        Token op = current();
+        int prec = precedence(op);
+
+        if (prec < minPrec)
+            break;
+
+        advance();
+
+        ASTNode *right = parseAtom();
+
+        Token next = current();
+        int nextPrec = precedence(next);
+
+        if (nextPrec > prec) {
+            right = parseExpression(prec + 1);
+        }
+
+        ASTNode *node = malloc(sizeof(ASTNode));
+        node->type = AST_BINARY_EXPR;
+        node->left = left;
+        node->right = right;
+        node->value = op.value;
+
+        left = node;
+    }
+
+    return left;
+}
+
+
+
+static void pushProgram(Statement node) {
+    if (program.count >= program.capacity) {
+        program.capacity = program.capacity == 0 ? 8 : program.capacity * 2;
+        program.body = realloc(program.body, sizeof(ASTNode) * program.capacity);
+        
+        if (!program.body) {
+            raise("Memory allocation failed", current().line, current().collumn);
+            exit(1);
+        }
+    }
+
+    program.body[program.count++] = node;
+}
+
+static void pushStatement(Statement *stmt, ASTNode node) {
+    if (stmt->capacity == 0) {
+        stmt->capacity = 8;
+        stmt->body = malloc(sizeof(ASTNode) * stmt->capacity);
+        if (!stmt->body) {
+            raise("Memory allocation failed", 0, 0);
+            exit(1);
+        }
+    }
+
+    if (stmt->count >= stmt->capacity) {
+        LOG("REALLOC: count=%d capacity=%d", stmt->count, stmt->capacity);
+        stmt->capacity *= 2;
+        LOG("REALLOC pt2: count=%d capacity=%d", stmt->count, stmt->capacity);
+        stmt->body = realloc(stmt->body, sizeof(ASTNode) * stmt->capacity);
+
+        if (!stmt->body) {
+            raise("Memory allocation failed", 0, 0);
+            exit(1);
+        }
+    }
+
+    stmt->body[stmt->count++] = node;
+}
+
+static ASTNode vardefnode(char *value) {
+    LOG("Node made");
+    ASTNode vardef = {0};
+    vardef.type = AST_VAR_DECL;
+    vardef.value = value;
+    return vardef;
+}
+
+static ASTNode identnode(char *value) {
+    LOG("Node made");
+    ASTNode vardef = {0};
+    vardef.type = AST_IDENTIFIER;
+    vardef.value = value;
+    return vardef;
+}
+
+
+
+void parseVarDef(Statement *stmt) {
+    stmt->type = STMT_VAR_DECL;
+    pushStatement(stmt, vardefnode(current().value));
+    expect(IDENTIFIER, "Invalid Sequence For Variable Declaration");
+    pushStatement(stmt, identnode(current().value));
+    expect(EQUALS, "Invalid Sequence For Variable Declaration");
+    advance();
+    ASTNode *expr = parseExpression(0);
+    pushStatement(stmt, *expr);
+
+    // current() is now on ';' — check it directly
+    if (current().type != SEMICOLON) {
+        raise("Did not find semicolon after statement.", current().line, current().collumn);
+        exit(1);
+    }
+    advance(); // move past ';'
+    return;
+}
+
+
+void parseIdentifier(Statement *stmt) {
+    LOG("Ident");
+    if (strIsVarDef(current().value)) {
+        parseVarDef(stmt);
+        return;
+    } else {
+        raise("Unkown Identifier In Body", current().line, current().collumn);
+    }
+}
+
+
+Statement parseStatement() {
+    LOG("STMT");
+    Statement stmt = {0};
+    stmt.type = STMT_NULL;
+    stmt.count = 0;
+    stmt.capacity = 0;
+    stmt.startl = current().line;
+    stmt.startc = current().collumn;
+    stmt.endl = current().line;
+    stmt.endc = current().collumn;
+
+    switch (current().type) {
+        case IDENTIFIER:
+            parseIdentifier(&stmt);
+            break;
+        
+        default:
+            raise("Unknown Token Type", current().line, current().collumn);
+            break;
+    }
+
+    stmt.endl = current().line;
+    stmt.endc = current().collumn;
+    return stmt;
+}
+
+Program parse(TokenStream tokens) {
+    LOG("PROGRAM");
+    p = &parser;
+    p->tokens = tokens.stream;
+    p->count = tokens.count;
+    p->pos = 0;
+    
+    while (current().type != ENDOFSTREAM) {
+        LOG("PROGRAM LOOP pos=%d token=%s", p->pos, current().value);
+        pushProgram(parseStatement());
+    }
+
+    return program;
+}
+
--- src/parser.c
+++ src/parser.c
@@ -1,4 +1,3 @@
-
 #include "../include/type.h"
 #include "../include/errors.h"
 #include "../include/nodes.h"
@@ -6,19 +5,22 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-#define DEBUG true
-#define LOG(fmt, ...) \
-    if (DEBUG) printf("[LOG] " fmt "\n", ##__VA_ARGS__)
-
-static ASTNode *parseExpression(int minPrec);
-static ASTNode *parseAtom();
+#include <stdint.h>
 
 typedef struct {
     Token *tokens;
     int pos;
     int count;
-} Parser;
+    uint8_t bytebuff[2048];
+    int byteIndex;
+    int hasHalfByte;
+    uint8_t halfByte;
+} ByteCoder;
+
+typedef struct {
+    uint8_t *data;
+    int length;
+} ByteCodeResult;
 
 static char *VARDEFS[] = {
     "str",
@@ -27,39 +29,70 @@ static char *VARDEFS[] = {
 };
 static int VARDEFCOUNT = 3;
 
+ByteCoder bytecoder = {0};
+ByteCoder *b;
 
-
-Parser parser;
-Parser *p;
-Program program = {0};
 
 
 static Token current() {
-    LOG("current");
-    return p->tokens[p->pos];
+    return b->tokens[b->pos];
 }
 
 static Token peek() {
-    LOG("peek");
-    return p->tokens[p->pos + 1];
+    return b->tokens[b->pos+1];
 }
 
 static void advance() {
-    LOG("advnce");
-    p->pos++;
-    LOG("PROGRAM LOOP pos=%d token=%s", p->pos, current().value);
+    b->pos++;
 }
 
 static void expect(TokenType type, char *errorStr) {
-    LOG("except");
     if (type != peek().type) {
         raise(errorStr, peek().line, peek().collumn);
     }
     advance();
 }
 
+static void expectAndPass(TokenType type, char *errorStr) {
+    if (type != peek().type) {
+        raise(errorStr, peek().line, peek().collumn);
+    }
+    advance();
+    advance();
+}
 
-static bool strIsVarDef(char *str) {
+static void writeByte(uint8_t value) {
+    if ((size_t)b->byteIndex >= sizeof(b->bytebuff)) {
+        raise("Byte buffer overflow", current().line, current().collumn);
+    }
+
+    b->bytebuff[b->byteIndex] = value;
+    b->byteIndex++;
+}
+
+static void writeHalfByte(uint8_t value) {
+    if (!b->hasHalfByte) {
+        b->halfByte = value & 0xF;
+        b->hasHalfByte = 1;
+    } else {
+        uint8_t full = (b->halfByte << 4) | (value & 0xF);
+        writeByte(full);
+        b->hasHalfByte = 0;
+    }
+}
+
+static void writeRawExpr(char *expr, uint8_t opcode) {
+    uint8_t len = (uint8_t)strlen(expr);
+
+    writeByte(opcode);
+
+    for (uint8_t i = 0; i < len; i++) {
+        writeByte((uint8_t)expr[i]);
+    }
+    writeByte(0xFF);
+}
+
+static bool strInVarDef(char *str) {
     for (int i = 0; i < VARDEFCOUNT; i++) {
         if (strcmp(VARDEFS[i], str) == 0) {
             return true;
@@ -68,218 +101,78 @@ static bool strIsVarDef(char *str) {
     return false;
 }
 
-
-static int precedence(Token tok) {
-    if (tok.type != OPERATION) return -1;
-
-    if (strcmp(tok.value, "+") == 0) return 1;
-    if (strcmp(tok.value, "-") == 0) return 1;
-    if (strcmp(tok.value, "*") == 0) return 2;
-    if (strcmp(tok.value, "/") == 0) return 2;
-
-    return -1;
-}
-
-
-static ASTNode *parseAtom() { // smallest possible node
-    LOG("parse atom");
-    Token tok = current();
-    ASTNode *node = malloc(sizeof(ASTNode));
-
-    switch (tok.type) {
-        case NUMBER:
-            node->type = AST_NUMBER;
-            node->value = tok.value;
-            advance();
-            return node;
-
-        case IDENTIFIER:
-            node->type = AST_IDENTIFIER;
-            node->value = tok.value;
-            advance();
-            return node;
-
-        case OPENBRAC: // '('
-            advance();
-
-            ASTNode *expr = parseExpression(0);
-
-            if (current().type != CLOSEBRAC) {
-                raise("Expected ')'", tok.line, tok.collumn);
-                exit(1);
-            }
-
-            advance();
-            return expr;
-
-        default:
-            raise("Expected expression", tok.line, tok.collumn);
-            exit(1);
-    }
-}
-
-static ASTNode *parseExpression(int minPrec) {
-    LOG("parse expr");
-    ASTNode *left = parseAtom();
-
-    while (true) {
-        Token op = current();
-        int prec = precedence(op);
-
-        if (prec < minPrec)
-            break;
-
-        advance();
-
-        ASTNode *right = parseAtom();
-
-        Token next = current();
-        int nextPrec = precedence(next);
-
-        if (nextPrec > prec) {
-            right = parseExpression(prec + 1);
-        }
-
-        ASTNode *node = malloc(sizeof(ASTNode));
-        node->type = AST_BINARY_EXPR;
-        node->left = left;
-        node->right = right;
-        node->value = op.value;
-
-        left = node;
+static void parseVarDecl() {
+    writeHalfByte(0x1);
+    int arrOff = 0;
+    if (strcmp(peek().value, "arr") == 0) {
+        arrOff = 1;
     }
 
-    return left;
-}
-
-
-
-static void pushProgram(Statement node) {
-    if (program.count >= program.capacity) {
-        program.capacity = program.capacity == 0 ? 8 : program.capacity * 2;
-        program.body = realloc(program.body, sizeof(ASTNode) * program.capacity);
-        
-        if (!program.body) {
-            raise("Memory allocation failed", current().line, current().collumn);
-            exit(1);
-        }
-    }
-
-    program.body[program.count++] = node;
-}
-
-static void pushStatement(Statement *stmt, ASTNode node) {
-    if (stmt->capacity == 0) {
-        stmt->capacity = 8;
-        stmt->body = malloc(sizeof(ASTNode) * stmt->capacity);
-        if (!stmt->body) {
-            raise("Memory allocation failed", 0, 0);
-            exit(1);
-        }
-    }
-
-    if (stmt->count >= stmt->capacity) {
-        LOG("REALLOC: count=%d capacity=%d", stmt->count, stmt->capacity);
-        stmt->capacity *= 2;
-        LOG("REALLOC pt2: count=%d capacity=%d", stmt->count, stmt->capacity);
-        stmt->body = realloc(stmt->body, sizeof(ASTNode) * stmt->capacity);
-
-        if (!stmt->body) {
-            raise("Memory allocation failed", 0, 0);
-            exit(1);
-        }
-    }
-
-    stmt->body[stmt->count++] = node;
-}
-
-static ASTNode vardefnode(char *value) {
-    LOG("Node made");
-    ASTNode vardef = {0};
-    vardef.type = AST_VAR_DECL;
-    vardef.value = value;
-    return vardef;
-}
-
-static ASTNode identnode(char *value) {
-    LOG("Node made");
-    ASTNode vardef = {0};
-    vardef.type = AST_IDENTIFIER;
-    vardef.value = value;
-    return vardef;
-}
-
-
-
-void parseVarDef(Statement *stmt) {
-    stmt->type = STMT_VAR_DECL;
-    pushStatement(stmt, vardefnode(current().value));
-    expect(IDENTIFIER, "Invalid Sequence For Variable Declaration");
-    pushStatement(stmt, identnode(current().value));
-    expect(EQUALS, "Invalid Sequence For Variable Declaration");
-    advance();
-    ASTNode *expr = parseExpression(0);
-    pushStatement(stmt, *expr);
-
-    // current() is now on ';' — check it directly
-    if (current().type != SEMICOLON) {
-        raise("Did not find semicolon after statement.", current().line, current().collumn);
-        exit(1);
-    }
-    advance(); // move past ';'
-    return;
-}
-
-
-void parseIdentifier(Statement *stmt) {
-    LOG("Ident");
-    if (strIsVarDef(current().value)) {
-        parseVarDef(stmt);
-        return;
+    if (strcmp(current().value, "int") == 0) {
+        writeHalfByte(0x1 + arrOff);
     } else {
-        raise("Unkown Identifier In Body", current().line, current().collumn);
+        raise("Internal Parser Error - Dead Var Decl",
+            current().line,
+            current().collumn);
     }
+
+    if (arrOff) {
+        advance(); // int arr *x*
+    }
+    expect(IDENTIFIER, "No identifier after var decl");
+    writeRawExpr(current().value, 0xFE);
+    expectAndPass(EQUALS, "No equals after var decl identifier");
+
+    char expr[1024];
+    int exprLoc = 0;
+
+    while (current().type != SEMICOLON) {
+        char *val = current().value;
+        for (int j = 0; val[j] != '\0'; j++) {
+            expr[exprLoc++] = val[j];
+        }
+        advance();
+    }
+
+    expr[exprLoc] = '\0';
+    writeRawExpr(expr, 0xFD);
+
+    advance(); // past semicolon
 }
 
-
-Statement parseStatement() {
-    LOG("STMT");
-    Statement stmt = {0};
-    stmt.type = STMT_NULL;
-    stmt.count = 0;
-    stmt.capacity = 0;
-    stmt.startl = current().line;
-    stmt.startc = current().collumn;
-    stmt.endl = current().line;
-    stmt.endc = current().collumn;
-
+static void parseStatement() {
     switch (current().type) {
         case IDENTIFIER:
-            parseIdentifier(&stmt);
+            if (strInVarDef(current().value)) {
+                parseVarDecl();
+            }
             break;
         
         default:
             raise("Unknown Token Type", current().line, current().collumn);
             break;
     }
-
-    stmt.endl = current().line;
-    stmt.endc = current().collumn;
-    return stmt;
+    writeByte(0xf0); // end of line
 }
 
-Program parse(TokenStream tokens) {
-    LOG("PROGRAM");
-    p = &parser;
-    p->tokens = tokens.stream;
-    p->count = tokens.count;
-    p->pos = 0;
-    
+ByteCodeResult parseToByteCode(TokenStream ts) {
+    b = &bytecoder;
+    b->tokens = ts.stream;
+    b->pos = 0;
+    b->byteIndex = 0;
+    b->hasHalfByte = 0;
+
     while (current().type != ENDOFSTREAM) {
-        LOG("PROGRAM LOOP pos=%d token=%s", p->pos, current().value);
-        pushProgram(parseStatement());
+        parseStatement();
     }
+    writeByte(0x00);
 
-    return program;
+    if (b->hasHalfByte) {
+        raise("Unpaired half-byte", current().line, current().collumn);
+        callAllErr();
+    }
+    ByteCodeResult res = {0};
+    res.data = b->bytebuff;
+    res.length = b->byteIndex;
+    return res;
 }
-
