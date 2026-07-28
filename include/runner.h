d410d34866f5b8da7df818a95a1d206c5e06cd80
Josh Ruddick <joshua.ruddick@icloud.com>
2026-07-28 17:52:29 +0200

    document runner

--- include/runner.h
+++ include/runner.h
@@ -9,6 +9,11 @@
 #ifndef RUNNER_H
 #define RUNNER_H
 
+/// @brief Run the given program.
+/// @param filename The file to run.
+/// @param verbose Describes how much to output.
+/// @retval 0 Success.
+/// @retval 1 Failure.
 int run(char *filename, bool verbose);
 
 #endif
